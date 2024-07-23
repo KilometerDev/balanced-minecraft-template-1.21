3b537a3d7b74b8b695c094ffcdba282d3d89f801
KilometerDev <milesbuss7@gmail.com>
2024-07-22 22:54:02 -0500

    Initial commit

---
+++ src/main/java/net/miles/balancedminecraft/BalancedMinecraftClient.java
@@ -0,0 +1,10 @@
+package net.miles.balancedminecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BalancedMinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
